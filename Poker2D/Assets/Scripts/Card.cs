ef16b7cf50220f22c134f23e464738183b09efc8
BMajeric <borna.majeric@gmail.com>
2025-04-13 02:48:00 +0200

    Adding code that throws an error to keep it for later adjustment and further testing

--- Poker2D/Assets/Scripts/Card.cs
+++ Poker2D/Assets/Scripts/Card.cs
@@ -4,8 +4,7 @@ public class Card
 {
     public Suit Suit { get; private set; }
     public Rank Rank { get; private set; }
-
-    // public Sprite cardSprite { get; private set; }
+    public Sprite cardSprite { get; private set; }
 
     public Card(Rank rank, Suit suit)
     {
--- Poker2D/Assets/Scripts/GameManager.cs
+++ Poker2D/Assets/Scripts/GameManager.cs
@@ -1,17 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject cardPrefab;
+    [SerializeField] private List<Transform> handCardTransform;
+
     private Deck _deck;
+    private Dictionary<string, Sprite> _cardSprites;
 
     void Start()
     {
+        // Load sprites of cards into dictionary
+        //_cardSprites = LoadCardSprites();
+
         _deck = new Deck();
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 2; i++)
         {
             _deck.DrawCard();
+            //GameObject drawnCard = Instantiate(cardPrefab, handCardTransform[i]);
+            //drawnCard.transform.GetChild(0).transform.GetChild(0).GetComponent<Image>().sprite = _cardSprites["Ace_of_Spades"];
         }
+    }
+
+    private Dictionary<string, Sprite> LoadCardSprites()
+    {
+        Dictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
+        Sprite[] loadedSprites = Resources.LoadAll<Sprite>("CardSprites");
+
+        foreach(Sprite sprite in loadedSprites)
+        {
+            sprites[sprite.name] = sprite;
+        }
+
+        return sprites;
     }
 
 }
