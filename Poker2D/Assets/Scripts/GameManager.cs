f19b38ec473446b9c051796da5f215892db33f6b
BMajeric <borna.majeric@gmail.com>
2025-04-16 20:45:55 +0200

    Added card dealing positions to scene for enemy hand, flop, turn and river

--- Poker2D/Assets/Scripts/GameManager.cs
+++ Poker2D/Assets/Scripts/GameManager.cs
@@ -5,7 +5,10 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject cardPrefab;
-    [SerializeField] private List<Transform> handCardTransform;
+    [SerializeField] private List<Transform> handCardTransforms;
+    [SerializeField] private List<Transform> flopCardTransforms;
+    [SerializeField] private Transform turnCardTransform;
+    [SerializeField] private Transform riverCardTransform;
 
     private Deck _deck;
     private Dictionary<string, Sprite> _cardSprites;
@@ -16,14 +19,35 @@ public class GameManager : MonoBehaviour
         Dictionary<string, Sprite> _cardSprites = LoadCardSprites();
 
         _deck = new Deck(_cardSprites);
-
-        for (int i = 0; i < 2; i++)
+        
+        // Hand dealing
+        for (int i = 0; i < handCardTransforms.Count; i++)
         {
             Card drawnCard = _deck.DrawCard();
-            GameObject drawnCardGameObject = Instantiate(cardPrefab, handCardTransform[i]);
+            GameObject drawnCardGameObject = Instantiate(cardPrefab, handCardTransforms[i]);
             drawnCardGameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Image>().sprite = drawnCard.CardSprite;
             drawnCardGameObject.GetComponentInChildren<Canvas>().worldCamera = Camera.main;
         }
+
+        //// TEST: flop, turn and river draw
+        //// Flop
+        //for (int i = 0; i < flopCardTransforms.Count; i++)
+        //{
+        //    Card flopCard = _deck.DrawCard();
+        //    GameObject flopCardGameObject = Instantiate(cardPrefab, flopCardTransforms[i]);
+        //    flopCardGameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Image>().sprite = flopCard.CardSprite;
+        //    flopCardGameObject.GetComponentInChildren<Canvas>().worldCamera = Camera.main;
+        //}
+        //// Turn
+        //Card turnCard = _deck.DrawCard();
+        //GameObject turnCardGameObject = Instantiate(cardPrefab, turnCardTransform);
+        //turnCardGameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Image>().sprite = turnCard.CardSprite;
+        //turnCardGameObject.GetComponentInChildren<Canvas>().worldCamera = Camera.main;
+        //// River
+        //Card riverCard = _deck.DrawCard();
+        //GameObject riverCardGameObject = Instantiate(cardPrefab, riverCardTransform);
+        //riverCardGameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Image>().sprite = riverCard.CardSprite;
+        //riverCardGameObject.GetComponentInChildren<Canvas>().worldCamera = Camera.main;
     }
 
     private Dictionary<string, Sprite> LoadCardSprites()
